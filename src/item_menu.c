bb1de41ce6a1b73d26d4174d2d98cb42174a7efe
Hedara <hedara90@gmail.com>
2026-08-03 10:23:41 +0200

    Remove item use outside of battle

--- src/item_menu.c
+++ src/item_menu.c
@@ -314,8 +314,8 @@ static const struct MenuAction sItemMenuActions[] = {
 // these are all 2D arrays with a width of 2 but are represented as 1D arrays
 // ACTION_DUMMY is used to represent blank spaces
 static const u8 sContextMenuItems_ItemsPocket[] = {
-    ACTION_USE,         ACTION_GIVE,
-    ACTION_TOSS,        ACTION_CANCEL
+    //ACTION_USE,         ACTION_GIVE,
+    ACTION_GIVE,        ACTION_CANCEL
 };
 
 static const u8 sContextMenuItems_KeyItemsPocket[] = {
@@ -334,9 +334,9 @@ static const u8 sContextMenuItems_TmHmPocket[] = {
 };
 
 static const u8 sContextMenuItems_BerriesPocket[] = {
-    ACTION_CHECK_TAG,   ACTION_DUMMY,
-    ACTION_USE,         ACTION_GIVE,
-    ACTION_TOSS,        ACTION_CANCEL
+    //ACTION_CHECK_TAG,   ACTION_DUMMY,
+    //ACTION_USE,         ACTION_GIVE,
+    ACTION_GIVE,        ACTION_CANCEL
 };
 
 static const u8 sContextMenuItems_BattleUse[] = {
