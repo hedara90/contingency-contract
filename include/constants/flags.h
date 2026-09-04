420840e60d6b361393755c002870b132305365e5
Turtleye15 <92760316+Turtleye15@users.noreply.github.com>
2026-09-04 17:36:17 +0200

    some more

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -54,9 +54,9 @@
 #define FLAG_QUEUE_LOADED    0x20
 #define FLAG_OTHER_LOADED    0x21
 #define FLAG_GLADIIA    0x22 // Unused Flag
-#define FLAG_UNUSED_0x023    0x23 // Unused Flag
-#define FLAG_UNUSED_0x024    0x24 // Unused Flag
-#define FLAG_UNUSED_0x025    0x25 // Unused Flag
+#define FLAG_MIFU_EVENT    0x23 // Unused Flag
+#define FLAG_CARNELIAN    0x24 // Unused Flag
+#define FLAG_JOHNENDFIELD    0x25 // Unused Flag
 #define FLAG_UNUSED_0x026    0x26 // Unused Flag
 #define FLAG_UNUSED_0x027    0x27 // Unused Flag
 #define FLAG_UNUSED_0x028    0x28 // Unused Flag
