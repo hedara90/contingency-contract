3b536125ef5d3e137862c3ad52594f1fad5fe587
Turtleye15 <92760316+Turtleye15@users.noreply.github.com>
2026-09-04 22:08:50 +0200

    CC intro script

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -57,7 +57,7 @@
 #define FLAG_MIFU_EVENT    0x23 // Unused Flag
 #define FLAG_CARNELIAN    0x24 // Unused Flag
 #define FLAG_JOHNENDFIELD    0x25 // Unused Flag
-#define FLAG_UNUSED_0x026    0x26 // Unused Flag
+#define FLAG_INTRO    0x26 // Unused Flag
 #define FLAG_UNUSED_0x027    0x27 // Unused Flag
 #define FLAG_UNUSED_0x028    0x28 // Unused Flag
 #define FLAG_UNUSED_0x029    0x29 // Unused Flag
