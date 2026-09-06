d457af38860f5487dc1d1ce61cc477c8f8085520
Turtleye15 <92760316+Turtleye15@users.noreply.github.com>
2026-09-06 16:02:42 +0200

    even more scripting

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -61,9 +61,9 @@
 #define FLAG_CHISA_CC    0x27 // Unused Flag
 #define FLAG_LAEVATAIN_SHOOT    0x28 // Unused Flag
 #define FLAG_LAEVATAIN_EATING    0x29 // Unused Flag
-#define FLAG_UNUSED_0x02A    0x2A // Unused Flag
-#define FLAG_UNUSED_0x02B    0x2B // Unused Flag
-#define FLAG_UNUSED_0x02C    0x2C // Unused Flag
+#define FLAG_SATSUKI    0x2A // Unused Flag
+#define FLAG_WEIQI    0x2B // Unused Flag
+#define FLAG_DRAGONS    0x2C // Unused Flag
 #define FLAG_UNUSED_0x02D    0x2D // Unused Flag
 #define FLAG_UNUSED_0x02E    0x2E // Unused Flag
 #define FLAG_UNUSED_0x02F    0x2F // Unused Flag
