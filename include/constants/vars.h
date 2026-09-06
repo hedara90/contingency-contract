7c9fedda61a3968dab6fe517ef1d1d064d55a681
Turtleye15 <92760316+Turtleye15@users.noreply.github.com>
2026-09-06 17:18:05 +0200

    save

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -270,7 +270,7 @@
 #define VAR_GLADIIA_WINS                                0x40F8 // Unused Var
 #define VAR_INTRO                                0x40F9 // Unused Var
 #define VAR_CHISA_PARTY                                0x40FA // Unused Var
-#define VAR_UNUSED_0x40FB                                0x40FB // Unused Var
+#define VAR_GAUNTLETHANDLING                                0x40FB // Unused Var
 #define VAR_UNUSED_0x40FC                                0x40FC // Unused Var
 #define VAR_UNUSED_0x40FD                                0x40FD // Unused Var
 #define VAR_UNUSED_0x40FE                                0x40FE // Unused Var
