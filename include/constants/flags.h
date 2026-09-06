e0b7f7ed8eb1471c17e91a17dfdf4bd28215d2a7
Pawkkie <61265402+Pawkkie@users.noreply.github.com>
2026-09-05 22:53:47 -0400

    Laev and Pog event adjustments (#123)
    * laev and pog adjustments
    
    * the heck
    
    * scrap hidden item for now

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -59,8 +59,8 @@
 #define FLAG_JOHNENDFIELD    0x25 // Unused Flag
 #define FLAG_INTRO    0x26 // Unused Flag
 #define FLAG_CHISA_CC    0x27 // Unused Flag
-#define FLAG_UNUSED_0x028    0x28 // Unused Flag
-#define FLAG_UNUSED_0x029    0x29 // Unused Flag
+#define FLAG_LAEVATAIN_SHOOT    0x28 // Unused Flag
+#define FLAG_LAEVATAIN_EATING    0x29 // Unused Flag
 #define FLAG_UNUSED_0x02A    0x2A // Unused Flag
 #define FLAG_UNUSED_0x02B    0x2B // Unused Flag
 #define FLAG_UNUSED_0x02C    0x2C // Unused Flag
