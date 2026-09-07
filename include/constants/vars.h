ee7ae1f18cb0ff98eb02e817bed9597d17a2de91
Turtleye15 <92760316+Turtleye15@users.noreply.github.com>
2026-09-07 10:40:46 +0200

    Music script

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -271,7 +271,7 @@
 #define VAR_INTRO                                0x40F9 // Unused Var
 #define VAR_CHISA_PARTY                                0x40FA // Unused Var
 #define VAR_GAUNTLETHANDLING                                0x40FB // Unused Var
-#define VAR_UNUSED_0x40FC                                0x40FC // Unused Var
+#define VAR_MUSIC                                0x40FC // Unused Var
 #define VAR_UNUSED_0x40FD                                0x40FD // Unused Var
 #define VAR_UNUSED_0x40FE                                0x40FE // Unused Var
 #define VAR_UNUSED_0x40FF                                0x40FF // Unused Var
